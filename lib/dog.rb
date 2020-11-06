11eed4bbbdd05b9b659a2a645d6dc93029d22f81
binding-proc-8938 <naomirhames2020@gmail.com>
2020-11-06 02:26:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,6 @@
 class Dog
-  def name=(new_name)
+  def initialize(new_name)
     @name = new_name
-    fido = Dog.new("Fido")
-  fido.name
-fido.name=("Fido")
-fido.instance_variable_get(@name)
  end
 end
  
